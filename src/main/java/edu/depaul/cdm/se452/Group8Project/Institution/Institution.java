3f0dca7415b9289d6f971920885d8f9af61ee1e6
JohnSmillie <jsmillie23@gmail.com>
2024-04-14 16:29:37 -0500

    Institution folder and .java added

---
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/Institution/Institution.java
@@ -0,0 +1,10 @@
+package edu.depaul.cdm.se452.Group8Project.Institution;
+
+public class Institution {
+    private String name;
+
+    public Institution(String name){
+        this.name = name;
+    }
+
+}
