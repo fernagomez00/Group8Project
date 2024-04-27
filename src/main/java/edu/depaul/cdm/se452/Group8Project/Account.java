08fddc11d34a0a65a43058c1353c6ddff7b756d3
johnsmillie <jsmillie23@gmail.com>
2024-04-26 22:06:55 -0500

    abstract class account and sub class checkingaccount

--- src/main/java/edu/depaul/cdm/se452/Group8Project/Account.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/Account.java
@@ -1,52 +1,44 @@
 package edu.depaul.cdm.se452.Group8Project;
 
+import edu.depaul.cdm.se452.Group8Project.customer.Customer;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Data;
 import java.math.BigDecimal;
+import java.util.UUID;
 
-public class Account{
-    private long id;
-    private String firstname,lastname;
-    private BigDecimal checking;
-    private BigDecimal savings;
 
-    public Account(){}
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Account{
+    
+    @Id
+    @GeneratedValue
+    protected UUID id; // in-house number
+    protected int number; // customer
+    protected UUID primary; // the primary account holder
+    private BigDecimal balance;
+    protected int pin;
+    
+    
 
-    public long getID(){
-        return id;
+    
+   public Account(Customer customer, BigDecimal startingBalance, int bankAssignedNumber, int pin){
+        //TODO complete constructor
+        //this.primary = customer.getID();
+        this.balance = startingBalance;
+        this.number = bankAssignedNumber;
+        this.pin = pin;
+    } 
+
+    public BigDecimal deposit(BigDecimal amount){
+        //TODO update and return new balance
+        return null;
     }
+    
 
-    public String getFirstname(){
-        return firstname;
-    }
-
-    public String getLastname(){
-        return lastname;
-    }
-
-    public BigDecimal getChecking(){
-        return checking;
-    }
-
-    public BigDecimal getSavings(){
-        return savings;
-    }
-
-    public void setID(long id){
-        this.id = id;
-    }
-
-    public void setFirstname(String firstname){
-        this.firstname = firstname;
-    }
-
-    public void setLastname(String lastname){
-        this.lastname = lastname;
-    }
-
-    public void setChecking(BigDecimal checking){
-        this.checking = checking;
-    }
-
-    public void setSavings(BigDecimal savings){
-        this.savings = savings;
-    }
 }
--- src/main/java/edu/depaul/cdm/se452/Group8Project/checking/CheckingAccount.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/checking/CheckingAccount.java
@@ -1,5 +1,31 @@
 package edu.depaul.cdm.se452.Group8Project.checking;
 
-public class CheckingAccount {
+import java.math.BigDecimal;
+import edu.depaul.cdm.se452.Group8Project.Account;
+import edu.depaul.cdm.se452.Group8Project.customer.Customer;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class CheckingAccount extends Account{
+  private boolean debit;
+
+  public CheckingAccount(Customer customer, BigDecimal startingBalance, int bankAssignedNumber, int pin, boolean debit) {
+    super(customer, startingBalance, bankAssignedNumber, pin);
+    this.debit = debit;
+  }
   
+  public BigDecimal withdraw(BigDecimal amount){
+    //TODO update balance
+    return null;
+  }
+  public BigDecimal checkBalance(){
+    //TODO return balance
+    return null; //this.getBalance();
+  }
 }
