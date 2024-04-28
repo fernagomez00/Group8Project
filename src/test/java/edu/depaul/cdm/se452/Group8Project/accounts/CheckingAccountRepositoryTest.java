f2eb6fb55beaa4528bfc93a915dbcdeb75cf4c72
johnsmillie <jsmillie23@gmail.com>
2024-04-28 01:42:13 -0500

    checking account persistence tests started

---
+++ src/test/java/edu/depaul/cdm/se452/Group8Project/accounts/CheckingAccountRepositoryTest.java
@@ -0,0 +1,44 @@
+package edu.depaul.cdm.se452.Group8Project.accounts;
+import edu.depaul.cdm.se452.Group8Project.checking.CheckingAccount;
+import edu.depaul.cdm.se452.Group8Project.checking.CheckingAccountRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+public class CheckingAccountRepositoryTest {
+
+@Autowired
+private CheckingAccountRepository repo;
+
+  @Test
+  public void testLombok(){
+    CheckingAccount ca1 = new CheckingAccount();
+    ca1.setBalance(100.00);
+    Double x = ca1.getBalance();
+    CheckingAccount ca2 = new CheckingAccount(true);
+    boolean b = ca2.isDebit();
+    assertEquals("100.0", x.toString());
+    assertEquals(true, b);
+  }
+
+  @Test
+  public void testAddCheckingAccount(){
+    CheckingAccount ca1 = new CheckingAccount();
+    ca1.setId(1);
+    ca1.setBalance(100.00);
+    ca1.setDebit(false);
+    long b4 = repo.count();
+    repo.save(ca1);
+    long af = repo.count();
+    assertEquals(0, b4);
+    assertEquals(1, af);
+  }
+  
+}
