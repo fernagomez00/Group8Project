c894bd238e956409b3c955b7e3c443fce7a83b54
johnsmillie <jsmillie23@gmail.com>
2024-04-28 01:39:05 -0500

    simplified the data types and added annotations for persistence

--- src/main/java/edu/depaul/cdm/se452/Group8Project/Account.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/Account.java
@@ -1,44 +1,32 @@
 package edu.depaul.cdm.se452.Group8Project;
 
-import edu.depaul.cdm.se452.Group8Project.customer.Customer;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import java.math.BigDecimal;
-import java.util.UUID;
+import lombok.NoArgsConstructor;
+
 
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Account{
     
     @Id
-    @GeneratedValue
-    protected UUID id; // in-house number
-    protected int number; // customer
-    protected UUID primary; // the primary account holder
-    private BigDecimal balance;
-    protected int pin;
-    
-    
-
-    
-   public Account(Customer customer, BigDecimal startingBalance, int bankAssignedNumber, int pin){
-        //TODO complete constructor
-        //this.primary = customer.getID();
-        this.balance = startingBalance;
-        this.number = bankAssignedNumber;
-        this.pin = pin;
-    } 
-
-    public BigDecimal deposit(BigDecimal amount){
-        //TODO update and return new balance
-        return null;
-    }
-    
+    @GeneratedValue (strategy = GenerationType.SEQUENCE)
+    protected Integer id; // in-house number
+    private Double balance;
+    /*protected Integer number; // customer account number
+    protected Integer primary; // the primary account holder
+    protected Integer pin;
+    */
 
 }
