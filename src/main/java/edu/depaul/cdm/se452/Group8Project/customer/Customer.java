40646e6cdab60de1268fbd6426f6e4b82309d97a
Fernando <97924413+fernagomez00@users.noreply.github.com>
2024-04-30 12:46:17 -0500

    Add files via upload

--- src/main/java/edu/depaul/cdm/se452/Group8Project/customer/Customer.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/customer/Customer.java
@@ -1,5 +1,28 @@
 package edu.depaul.cdm.se452.Group8Project.customer;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@Table(name = "customer")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)  
+  private long id;
   
+  private String firstname;
+  private String lastname;
+
 }
+
