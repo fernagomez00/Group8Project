d08114cec80e3945544fe9576ea13b24d9966b8d
Dsolanki658 <166934166+Dsolanki658@users.noreply.github.com>
2024-04-30 22:29:09 -0500

    Update SavingsAccount.java

--- src/main/java/edu/depaul/cdm/se452/Group8Project/SavingsAccount.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/SavingsAccount.java
@@ -1,17 +1,28 @@
 import javax.persistence.Entity;
+import javax.persistence.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "savings")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class SavingsAccount extends Account {
-    private double interestRate;
+    private double intRate;
 
-    public SavingsAccount() {
+    public SavingsAccount(String customer, String openingDate, double balance, double intRate) {
+        super(customer, openingDate, balance);
+        this.intRate = intRate;
     }
 
-    public SavingsAccount(String customer, String openDate, double balance, double interestRate) {
-        super(customer, openDate, balance);
-        this.interestRate = interestRate;
-    }
-
-    // Getters and setters
-    // Other methods specific to SavingsAccount
+    // Will set Getters and setters
+    // Will add other methods specific to SavingsAccount
 }
