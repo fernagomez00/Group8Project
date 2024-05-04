6e096064d934dbc00797b398ff5a701541bc08d2
johnsmillie <jsmillie23@gmail.com>
2024-05-03 22:46:54 -0500

    first commit - bare bones functionality

--- src/main/java/edu/depaul/cdm/se452/Group8Project/checking/CheckingAccountService.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/checking/CheckingAccountService.java
@@ -1,5 +1,35 @@
 package edu.depaul.cdm.se452.Group8Project.checking;
 
+import java.util.List;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CheckingAccountService {
   
+  @Autowired
+  private CheckingAccountRepository repo;
+
+  //TODO add logging to methods
+
+  public List<CheckingAccount> list(){
+    List<CheckingAccount> listOfCA = StreamSupport.stream(repo.findAll().spliterator(), false).collect(Collectors.toList());
+    return listOfCA;
+  }
+
+  public CheckingAccount save(CheckingAccount account){
+    repo.save(account);
+    return account;
+  }
+
+  public void delete(long id){
+    repo.deleteById((int) id);
+  }
+
+
+
 }
