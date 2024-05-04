f9f21f9440c2f057d640a25c70c7bb01e300356c
johnsmillie <jsmillie23@gmail.com>
2024-05-03 23:25:39 -0500

    Update and delete tests added

--- src/test/java/edu/depaul/cdm/se452/Group8Project/CheckingAccount/CheckingAccountRepositoryTest.java
+++ src/test/java/edu/depaul/cdm/se452/Group8Project/CheckingAccount/CheckingAccountRepositoryTest.java
@@ -3,6 +3,10 @@ import edu.depaul.cdm.se452.Group8Project.checking.CheckingAccount;
 import edu.depaul.cdm.se452.Group8Project.checking.CheckingAccountRepository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+
+import org.hibernate.annotations.Check;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,7 +36,6 @@ private CheckingAccountRepository repo;
   @Test
   public void testAddCheckingAccount(){
     CheckingAccount ca1 = new CheckingAccount();
-    //ca1.setId(1);
     ca1.setAccountNumber(1234567);
     ca1.setPrimaryHolderId(5000);
     ca1.setBalance(100.00);
@@ -44,5 +47,35 @@ private CheckingAccountRepository repo;
     assertEquals(0, b4);
     assertEquals(1, af);
   }
-  
+
+  @Test
+  public void testUpdateCheckingAccount(){
+      CheckingAccount ca1 = new CheckingAccount();
+      ca1.setPrimaryHolderId(69);
+      repo.save(ca1);
+      CheckingAccount ca2 = repo.findById(1).orElse(new CheckingAccount());
+      ca2.setAccountNumber(12345);
+      ca2.setBalance(2000.00);
+      repo.save(ca2);
+      CheckingAccount ca3 = repo.findById(1).orElse(new CheckingAccount());
+      assertEquals(69, ca3.getPrimaryHolderId());
+      assertEquals(2000.0, ca3.getBalance()); 
+  }
+
+  @Test
+  public void testDeleteCheckingAccount(){
+    CheckingAccount ca1 = new CheckingAccount();
+    CheckingAccount ca2 = new CheckingAccount();
+    CheckingAccount ca3 = new CheckingAccount();
+    repo.save(ca1);
+    repo.save(ca2);
+    repo.save(ca3);
+    long before = repo.count();
+    repo.deleteById(1);
+    long after = repo.count();
+    assertEquals(before, 3);
+    assertEquals(after, 2);
+  }
+
+
 }
